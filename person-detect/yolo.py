52a4203c60653e425dfb791ab5b4fc695853ff92
Swastik Agrawal <swastikagrawal3@gmail.com>
2024-04-20 11:43:15 -0400

    add conf labels

--- person-detect/yolo.py
+++ person-detect/yolo.py
@@ -5,6 +5,7 @@ import numpy as np
 from PIL import Image
 import cv2
 import torch
+
 # Load a model
 def create_model(dataset: str = "coco8.yaml"):
     # model = YOLO("yolov8l.yaml")  # build a new model from scratch
@@ -25,7 +26,7 @@ video_path = "bruh.mp4"
 cap = cv2.VideoCapture('bruh.mp4')
 fps_cap = 30
 cap.set(cv2.CAP_PROP_FPS, fps_cap)
-output_path = "video.mp4"
+output_path = "./tmp/video.mp4"
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 new_video = cv2.VideoWriter(output_path, fourcc, fps_cap, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
 total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -44,25 +45,23 @@ while cap.isOpened():
         res = results[0]
         rows_to_remove = []
         ind = 0
+        conf = []
         for box in results[0].boxes:
-            if box.cls != 0:
+            if box.cls == 0:
                 rows_to_remove.append(ind)
+                conf.append(box.conf)
             ind += 1
         box_data = res.boxes.xyxy.tolist()
-        indices_to_keep = [i for i in range(box_data.size(0)) if i not in rows_to_remove]
-        try: rows_to_keep: torch.Tensor = torch.index_select(box_data, 0, torch.tensor(indices_to_keep))
-        except:
-            print(frame_count, total_frames, sep=" ")
-            break
-        for box in rows_to_keep:
-            x1, y1, x2, y2 = box.tolist()
+        new_box_data = [box_data[i] for i in range(len(box_data)) if i in rows_to_remove]
+        new_box_data_tensor = torch.tensor(new_box_data)
+        # rows_to_keep = torch.index_select(box_data, 0, torch.tensor(indices_to_keep))
+        for box,confidence in zip(new_box_data,conf):
+            x1, y1, x2, y2 = box
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-
+            label = f"Person: {confidence[0]:.2f}"
+            cv2.putText(frame, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
         new_video.write(frame)
- 
-        # cv2.imshow("YOLOv8 Inference", frame,)
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #     break
+    else: break
 
 new_video.release()
 # print("THIS IS  WORKING")
