d33f1f36e316066bf50f19a30876921e67548d96
Swastik Agrawal <swastikagrawal3@gmail.com>
2024-04-20 17:41:53 -0400

    fixed some bs

--- react-frontend/src/ImageUpload.js
+++ react-frontend/src/ImageUpload.js
@@ -50,83 +50,8 @@ const ImageUpload = () => {
     }
   };
 
-  const handleCoordinateChange = (e) => {
-    setCoordinates(e.target.value);
-  };
-
-  const handleSubmit = async () => {
-    if (!image || !coordinates) {
-      console.error('Please upload an image and provide coordinates.');
-      return;
-    }
-
-    try {
-      const response = await fetch('http://127.0.0.1:5000/process-image', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ image, coordinates }),
-      });
-
-      if (response.ok) {
-        console.log('Image uploaded and processed successfully.');
-        // Handle success
-      } else {
-        console.error('Failed to upload image:', response.statusText);
-        // Handle error
-      }
-    } catch (error) {
-      console.error('Error uploading image:', error.message);
-      // Handle error
-    }
-  };
-
   return (
     <div>
-      <div className="empty"></div>
-      <div className="image-upload-container">
-        <h2 className="upload-title">Upload Your Image</h2>
-        <div className="upload-input-container">
-          <label htmlFor="upload-input" className="upload-label">
-            <input
-              type="file"
-              accept="image/*"
-              id="upload-input"
-              onChange={handleImageChange}
-              className="upload-input"
-            />
-            <FontAwesomeIcon icon={faCloudUploadAlt} className="upload-icon" />
-            Choose Image
-          </label>
-        </div>
-        <div className="coordinate-input-container">
-          <label htmlFor="coordinate-input" className="coordinate-label">
-            Coordinates:
-            <input
-              type="text"
-              id="coordinate-input"
-              value={coordinates}
-              onChange={handleCoordinateChange}
-              className="coordinate-input"
-            />
-          </label>
-        </div>
-        {image && (
-          <div className="image-preview">
-            <h3 className="preview-title">Preview</h3>
-            <img src={`data:image/jpeg;base64,${image}`} alt="Uploaded" className="preview-image" />
-          </div>
-        )}
-        <button onClick={handleSubmit} className="submit-button">Submit</button>
-      </div>
-    <div classname="empty">
-     
-
-
-
-     
-    </div>
     <div className="image-upload-container">
       <h2 className="upload-title">Upload Your Image</h2>
       <div className="upload-input-container">
