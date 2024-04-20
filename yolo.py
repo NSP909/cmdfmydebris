9dd0e337a05b25c89e67fed5c3e51ad96afa3a3a
Swastik Agrawal <swastikagrawal3@gmail.com>
2024-04-20 13:10:01 -0400

    integrated

--- yolo.py
+++ yolo.py
@@ -10,7 +10,6 @@ import torch
 def create_model(dataset: str = "coco8.yaml"):
     # model = YOLO("yolov8l.yaml")  # build a new model from scratch
     model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
-
     # Use the model
     model.train(data="coco8.yaml", epochs=3)  # train the model
     metrics = model.val()  # evaluate model performance on the validation set
@@ -34,9 +33,7 @@ def detect_person(video_path:str):
     while cap.isOpened():
         # Read a frame from the video
         print("FRAME = ", frame_count)
-
         success, frame = cap.read()
-
         frame_count += 1
         if success:
             # Run YOLOv8 inference on the frame
