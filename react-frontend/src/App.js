cf2a736920c0a62f3064bc9c13a4c3d72c51d4e4
AnuraagTripathy <tripathyanuraag@gmail.com>
2024-04-20 02:02:25 -0400

    routes

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
 import ImageUpload from './ImageUpload';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React Image Upload</h1>
-      </header>
-      <main>
-        <ImageUpload />
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>React Image Upload</h1>
+        </header>
+        <main>
+          <ImageUpload />
+        </main>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
