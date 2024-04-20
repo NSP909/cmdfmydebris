88d6bf14d6b539d476285c0c30e3d8aed08d3ba4
preethiN2 <pnaray2504@gmail.com>
2024-04-20 02:07:22 -0400

    made a new file

---
+++ react-frontend/src/SearchRescue.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ImageUpload from './ImageUpload';
+import './SearchRescue.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Image Upload</h1>
+      </header>
+      <main>
+        <ImageUpload />
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
