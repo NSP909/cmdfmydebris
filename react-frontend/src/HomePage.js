9759373423d6a95d42d97008abb4cb90a8062f28
AnuraagTripathy <tripathyanuraag@gmail.com>
2024-04-20 13:21:57 -0400

    box box

--- react-frontend/src/HomePage.js
+++ react-frontend/src/HomePage.js
@@ -12,7 +12,9 @@ const HomePage = () => {
       </header>
       <section className="purpose-section">
         <h2>Our Purpose</h2>
-        <p>This is the purpose section. Describe the purpose of your website here.</p>
+        <p>The purpose of this website is to show our invention of how we can help those in natural disaster situations. With the use of drone
+            footage we can see who is affected and find ways to help them. At the same time, we are also able to indentify the type of debris and 
+            how much of the debris is present at the site of the disaster inorder to come with solutions to fix this issue.</p>
       </section>
       <section className="buttons-section">
         <Link to="/search-rescue">
