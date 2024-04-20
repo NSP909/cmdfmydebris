ce5fd34030a8c6aa04fbf53b40e91ea03b7134f0
AnuraagTripathy <tripathyanuraag@gmail.com>
2024-04-20 02:59:29 -0400

    sampletext

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
 import SearchRescue from './SearchRescue'
-import ImageUpload from './ImageUpload';
 
 import './App.css';
 
@@ -11,7 +9,6 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path='/home' element = {<Home/>} />
           <Route path="/search-rescue" element={<SearchRescue />} />
         </Routes>
       </div>
